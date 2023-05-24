0b5845ec5e44e3987c0cec82038114e7d0d23d57
Nobilis <nikolaev-evgeny@yandex.ru>
2023-05-24 19:54:36 +0300

    2023-05-24/22.52 PGNReader Первая относительная удачная попытка итерирования по списку строк и группировки строк в партии.

--- src/main/java/PGNEditor.java
+++ src/main/java/PGNEditor.java
@@ -1,9 +1,18 @@
+import dao.PGNParser;
 import dao.PGNReader;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class PGNEditor {
 
     public static void main(String[] args) {
-        PGNReader.getPGNList("C:\\Users\\Nobilis\\YandexDisk-Nobilis13\\Шахматы\\bases\\новая.pgn");
+        PGNReader.getPGNList("C:\\Users\\Nobilis\\YandexDisk\\Шахматы\\bases\\Новая\\Надареишвили_ЭТЮД ГЛАЗАМИ ГРОССМЕЙСТЕРОВ.pgn");
+        //PGNParser.parsePGN()
+
+
+
+
     }
 
 }
--- src/main/java/dao/PGNReader.java
+++ src/main/java/dao/PGNReader.java
@@ -1,28 +1,91 @@
 package dao;
 
-import model.PGN;
+import model.Game;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.Duration;
-import java.time.Instant;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class PGNReader {
 
-    public static List<PGN> getPGNList(String path) {
-        StringBuilder sb = new StringBuilder("");
-        try(FileReader reader = new FileReader(path))
-        {
-            int c;
-            while((c=reader.read())!=-1){
-                sb.append((char)c);
-            }
+    private static LinkedList<String> lines;
+    private static List<List<String>> games;
+
+
+    public static List<Game> getPGNList(String path) {
+        lines = new LinkedList<>();
+        games = new LinkedList<>();
+        readByLines(path);
+        writeGames();
+
+        for (List<String> list: games
+             ) {
+            System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n-----------------------------------------------------------------------------------------------------------");
+            System.out.println(list);
         }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
+
         return null;
+    }
+
+    private static void readByLines(String path) {
+        File file = new File(path);
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            reader.lines().forEach(s -> lines.add(s.trim()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeGames() {
+
+        ListIterator<String> itr = lines.listIterator(0);
+        while(itr.hasNext()) {
+            String line = itr.next();
+            List<String> game = new ArrayList<>();
+            while(itr.hasNext() && line.startsWith("[") ) {
+                game.add(line);
+                line = itr.next();
+            }
+            while(itr.hasNext() && !itr.next().startsWith("[")) {
+                game.add(itr.next());
+            }
+            games.add(game);
+        }
+
+
+
+//        ListIterator<String> itr = lines.listIterator(0);
+//        List<String> game = new ArrayList<>();
+//        boolean tagsComplete = false;
+//        boolean isAdded = false;
+//
+//        while (itr.hasNext()) {
+//            String line = itr.next();
+//            if (line.length() > 0) {
+//                if (line.charAt(0) == '[') {
+//                    if(tagsComplete) {
+//                        if(!isAdded) {
+//                            games.add(game);
+//                        }
+//                        game = new ArrayList<>();
+//                    }
+//                    game.add(line);
+//                } else {
+//                    tagsComplete = true;
+//                    if(line.length() > 1) {
+//                        game.add(line);
+//                        isAdded = true;
+//                    }
+//                }
+//            }
+//        }
     }
 
 }
