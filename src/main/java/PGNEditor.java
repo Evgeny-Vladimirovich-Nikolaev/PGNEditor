0568515a5048dc643b38923d388643c15e98fcc7
Nobilis <nikolaev-evgeny@yandex.ru>
2023-05-28 21:36:00 +0300

    2023-05-28/20.55 PGNParser Исправление парсинга подстроки, содержащей значение тегов

--- src/main/java/PGNEditor.java
+++ src/main/java/PGNEditor.java
@@ -7,8 +7,10 @@ import java.util.Map;
 public class PGNEditor {
 
     public static void main(String[] args) {
-        PGNReader.getPGNList("C:\\Users\\Nobilis\\YandexDisk\\Шахматы\\bases\\Новая\\Надареишвили_ЭТЮД ГЛАЗАМИ ГРОССМЕЙСТЕРОВ.pgn");
+        PGNReader.getPGNList("C:\\Users\\Nobilis\\YandexDisk-Nobilis13\\Шахматы\\bases\\Новая\\Надареишвили_ЭТЮД ГЛАЗАМИ ГРОССМЕЙСТЕРОВ - копия.pgn");
         //PGNParser.parsePGN()
+
+
 
 
 
--- src/main/java/dao/PGNParser.java
+++ src/main/java/dao/PGNParser.java
@@ -1,6 +1,5 @@
 package dao;
 
-import com.sun.tools.javac.util.StringUtils;
 import model.Game;
 
 import java.util.ArrayList;
@@ -19,14 +18,27 @@ public class PGNParser {
                     case "[Date" :
                         addDate(game, line);
                         break;
-                    case "[Автор":
+                    case "[Автор" :
                         addAuthor(game, line);
                         break;
-                    case "[Event:" :
+                    case "[Event" :
                         addEvent(game, line);
                         break;
-                    case "[Задание":
+                    case "[Result" :
+                        addResult(game, line);
+                        break;
+                    case "[Задание" :
                         addTask(game, line);
+                        break;
+                    case "[Annotator" :
+                        addAnnotator(game, line);
+                        break;
+                    case "[Remark" :
+                        addRemark(game, line);
+                        break;
+                    case "[FEN" :
+                        addFen(game, line);
+                        analyzeFen(game, line);
                         break;
                 }
             }
@@ -35,19 +47,47 @@ public class PGNParser {
     }
 
     private static void addDate(Game game, String line) {
-        game.setDate(line.substring(line.indexOf('"'), line.lastIndexOf('"')));
+        game.setDate(line.substring(line.indexOf('"') +1, line.lastIndexOf('"')));
     }
 
     private static void addAuthor(Game game, String line) {
-        game.setAuthor(line.substring(line.indexOf('"'), line.lastIndexOf('"')));
+        game.setAuthor(line.substring(line.indexOf('"') +1, line.lastIndexOf('"')));
     }
 
     private static void addEvent(Game game, String line) {
-        game.setEvent(line.substring(line.indexOf('"'), line.lastIndexOf('"')));
+        game.setEvent(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
+    }
+
+    private static void addResult(Game game, String line) {
+        game.setResult(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
     }
 
     private static void addTask(Game game, String line) {
-        game.setTask(line.substring(line.indexOf('"'), line.lastIndexOf('"')));
+        game.setTask(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
     }
 
+    private static void addAnnotator(Game game, String line) {
+        game.setAnnotator(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
+    }
+
+    private static void addRemark(Game game, String line) {
+        game.setRemark(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
+    }
+
+    private static void addFen(Game game, String line) {
+        game.setFen(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
+    }
+
+    private static void analyzeFen(Game game, String fen) {
+        final String position = getPositionFromFen(fen);
+        calculateMaterial(game, position);
+    }
+
+    private static String getPositionFromFen(String fen) {
+        return null;
+    }
+
+    private static void calculateMaterial(Game game, String line) {
+
+    }
 }
