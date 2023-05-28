71383010a138345fb480be29d3654db8805f4431
Nobilis <nikolaev-evgeny@yandex.ru>
2023-05-28 23:21:18 +0300

    2023-05-28/23.20 PGNParser - Рефакторинг.

--- src/main/java/dao/PGNParser.java
+++ src/main/java/dao/PGNParser.java
@@ -14,68 +14,41 @@ public class PGNParser {
         for (List<String> list : processedList) {
             Game game = new Game();
             for (String line : list) {
-                switch (line.substring(0)) {
-                    case "[Date" :
-                        addDate(game, line);
-                        break;
-                    case "[Автор" :
-                        addAuthor(game, line);
-                        break;
-                    case "[Event" :
-                        addEvent(game, line);
-                        break;
-                    case "[Result" :
-                        addResult(game, line);
-                        break;
-                    case "[Задание" :
-                        addTask(game, line);
-                        break;
-                    case "[Annotator" :
-                        addAnnotator(game, line);
-                        break;
-                    case "[Remark" :
-                        addRemark(game, line);
-                        break;
-                    case "[FEN" :
-                        addFen(game, line);
-                        analyzeFen(game, line);
-                        break;
+                if (line.startsWith("[")) {
+                    String tagValue = line.substring(line.indexOf('"') +1, line.lastIndexOf('"'));
+                    switch (line.substring(0)) {
+                        case "[Date" :
+                            game.setDate(tagValue);
+                            break;
+                        case "[Автор" :
+                            game.setAuthor(tagValue);
+                            break;
+                        case "[Event" :
+                            game.setEvent(tagValue);
+                            break;
+                        case "[Result" :
+                            game.setResult(tagValue);
+                            break;
+                        case "[Задание" :
+                            game.setTask(line);
+                            break;
+                        case "[Annotator" :
+                            game.setAnnotator(line);
+                            break;
+                        case "[Remark" :
+                            game.setRemark(line);
+                            break;
+                        case "[FEN" :
+                            game.setFen(line);
+                            analyzeFen(game, line);
+                            break;
+                    }
+                } else {
+                    game.setBody(line);
                 }
             }
         }
         return null;
-    }
-
-    private static void addDate(Game game, String line) {
-        game.setDate(line.substring(line.indexOf('"') +1, line.lastIndexOf('"')));
-    }
-
-    private static void addAuthor(Game game, String line) {
-        game.setAuthor(line.substring(line.indexOf('"') +1, line.lastIndexOf('"')));
-    }
-
-    private static void addEvent(Game game, String line) {
-        game.setEvent(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
-    }
-
-    private static void addResult(Game game, String line) {
-        game.setResult(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
-    }
-
-    private static void addTask(Game game, String line) {
-        game.setTask(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
-    }
-
-    private static void addAnnotator(Game game, String line) {
-        game.setAnnotator(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
-    }
-
-    private static void addRemark(Game game, String line) {
-        game.setRemark(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
-    }
-
-    private static void addFen(Game game, String line) {
-        game.setFen(line.substring(line.indexOf('"') + 1, line.lastIndexOf('"')));
     }
 
     private static void analyzeFen(Game game, String fen) {
@@ -90,4 +63,5 @@ public class PGNParser {
     private static void calculateMaterial(Game game, String line) {
 
     }
+
 }
