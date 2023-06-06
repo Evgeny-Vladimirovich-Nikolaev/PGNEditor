bb7df1920df8d00202d28f0c729b3bda9397bdd0
Nobilis <nikolaev-evgeny@yandex.ru>
2023-06-06 15:22:43 +0300

    2023-06-06/15.20 PGNParser Реализовано распознавание большей части типов позиций, в т.ч. позиций с разноцветными и одноцветными слонами.

--- src/main/java/PGNEditor.java
+++ src/main/java/PGNEditor.java
@@ -1,3 +1,4 @@
+import dao.PGNParser;
 import dao.PGNReader;
 import model.Game;
 
@@ -6,15 +7,13 @@ import java.util.List;
 
 public class PGNEditor {
 
-    private static final String  PATH = "C:\\Users\\Nobilis\\YandexDisk\\JAVA\\ЭТЮД ГЛАЗАМИ ГРОССМЕЙСТЕРОВ.pgn";
+    private static final String  PATH = "C:\\Users\\Nobilis\\YandexDisk-Nobilis13\\JAVA\\ЭТЮД ГЛАЗАМИ ГРОССМЕЙСТЕРОВ.pgn";
 
     public static void main(String[] args) {
         List<Game> gamesList = PGNReader.getPGNList(PATH);
         //PGNParser.parsePGN()
 
-        System.out.println(gamesList.get(0));
-        System.out.println(gamesList.get(9));
-        System.out.println(gamesList.get(gamesList.size() - 1));
+
 
     }
 
